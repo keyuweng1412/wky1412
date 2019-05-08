bbe8d0d62834cc31dbdcebcd1262c977125f88ee
wengky <wengky@zjkeystone.com>
2019-05-08 00:42:33 -0400

    second

--- src/components/OrderManage.jsx
+++ src/components/OrderManage.jsx
@@ -1,60 +1,64 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
-
-var textname = ''
-var labelvalue = ''
+var count = 0
 class OrderManage extends Component{
-    
-    
+
     // state = {
     //     fruitsModel:['apple','pineapple','banana','strawberry'],
     //     prices:['11','12','13','14']
     // }
 
     handleChangeFruits = (e) => {
-        document.getElementById('label1').innerHTML = this.props.prices[e.target.value];
-        // 获取商品
-        var mySelect = document.getElementById('selected')
-        var index = mySelect.selectedIndex;
-        var textName = mySelect.options[index].text
-        textname = textName
-        console.log(textname);
+        count = e.target.value
+        document.getElementById('label1').innerHTML = this.props.prices[count];
+
+        // // 获取商品
+        // var mySelect = document.getElementById('selected')
+        // var index = mySelect.selectedIndex;
+        // var textName = mySelect.options[index].text
+        // textname = textName
+        // console.log(textname);
         
-        // 获取单价
-        var label = document.getElementById('label1')
-        var labelValue = label.innerText.trim()
-        labelvalue = labelValue
-        console.log(labelvalue);
+        // // 获取单价
+        // var label = document.getElementById('label1')
+        // var labelValue = label.innerText.trim()
+        // labelvalue = labelValue
+        // console.log(labelvalue);
     }
-    
+
     render(){
         const {prices} = this.props
         
         return(
             <div>
-                <select id='selected' onChange={this.handleChangeFruits}>
-                {
-                    this.props.fruitsModel.map((item, index) => {
-                        return <option value={index} key={item}>{item}</option>
-                    })
-                }
-                </select>
-                <label>单价/元:</label>
-                <label id='label1'>{prices[0]}</label>
-               
-                <input placeholder='请输入数量...' 
-                    value={this.props.inputValue}
-                    onChange={this.props.handleInputChange}></input>
-                <button onClick={this.props.clickBtn}>订购</button>
+                <fieldset>
+                    <select id='selected' onChange={this.handleChangeFruits}>
+                    {
+                        this.props.fruitsModel.map((item, index) => {
+                            return <option value={index} key={item}>{item}</option>
+                        })
+                    }
+                    </select>
+                    <label style={{marginLeft:'10px'}}>单价/元:</label>
+                    <label id='label1' style={{marginRight:'10px'}}>{prices[0]}</label>
+                    <input placeholder='请输入数量...' 
+                        value={this.props.inputValue}
+                        onChange={this.props.handleInputChange}></input>
+
+                    <button style={{marginLeft:'10px'}} onClick={this.props.clickBtn}>订购</button>
+
+                </fieldset>
                 
                 
                 <ul>
                 {
                     this.props.list.map((item,index)=>{
+                        console.log(item,index);
+                        return <li key={index}>
+                                    {item}
+                                </li>
                         
-
-                        return <li key={item}>{'商品名:' + textname + '单价:' + labelvalue + '数量:' + item + '总价:' + labelvalue*item}</li>
                     })
                 }
                 </ul>
@@ -75,7 +79,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps =(dispatch) => { 
     return {
-
+        
         // 输入框改变
         handleInputChange(e){
             // console.log(e.target.value)
@@ -85,16 +89,20 @@ const mapDispatchToProps =(dispatch) => {
             }
             dispatch(action)
         },
-        // 点击按钮
+        // 点击订购
         clickBtn(){
+
             const action = {
-                type: 'click_btn_add'
+                type: 'click_btn_add',
+                value: count
             }
             dispatch(action)
-        }
+        },
         
     }
 }
+
+
 export default connect(
     mapStateToProps,
     mapDispatchToProps
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -6,17 +6,41 @@
     }
 
 export default (state=defaultState, action) =>{
+
+    // if(action.type === 'click_dele_item'){
+    //     const newState = JSON.parse(JSON.stringify(state))
+    //     newState.list.splice(action.value, 1)
+    //     return newState
+    // }
     
     if(action.type === 'change_input_value'){
         const newState = JSON.parse(JSON.stringify(state))
         newState.inputValue = action.value
         return newState
     }
+
+
     if(action.type === 'click_btn_add'){
         const newState = JSON.parse(JSON.stringify(state))
-        newState.list.push(newState.inputValue);
+        if(!newState.inputValue){
+            alert('数量不能为空')
+            return newState
+        }
+        if(isNaN(newState.inputValue)){
+            alert('请输入数字')
+            newState.inputValue = ''
+            return newState
+        }
+
+        const str = '商品名:'+ newState.fruitsModel[action.value] + ',' +
+                     '单价:' + newState.prices[action.value] + ',' +
+                     '数量:' + newState.inputValue + ',' + 
+                     '总价:' + newState.prices[action.value]*newState.inputValue
+        newState.list.push(str);
         newState.inputValue = ''
         return newState
     }
+
+    
     return state
 }
